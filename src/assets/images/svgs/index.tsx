db5960a76ba4cd75e1ac73975882230d243f3748
Ajay <ajaygubhaju223@gmail.com>
2024-01-06 16:09:22 +0545

    feat:color and icon added

---
+++ src/assets/images/svgs/index.tsx
@@ -0,0 +1,12 @@
+import { ReactComponent as ArrowDownIcon } from './arrowDown.svg';
+import { ReactComponent as AccordianArrowIcon } from './accordArrow.svg';
+import { ReactComponent as CardOneIcon } from './card1Icon.svg';
+import { ReactComponent as CardTwoIcon } from './card2Icon.svg';
+import { ReactComponent as CardThreeIcon } from './card3Icon.svg';
+import { ReactComponent as CardFourIcon } from './card4Icon.svg';
+import { ReactComponent as AccordOneNFiveIcon } from './Acc1N5I.svg';
+import { ReactComponent as AccordTwoNFourIcon } from './Acc2N4I.svg';
+import { ReactComponent as AccordThreeIcon } from './Acc3I.svg';
+
+
+export const SVGS = { ArrowDownIcon, AccordianArrowIcon, CardOneIcon, CardTwoIcon, CardThreeIcon, CardFourIcon, AccordOneNFiveIcon, AccordTwoNFourIcon, AccordThreeIcon }
